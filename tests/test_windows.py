2d9ad2ee3e0af645a508d110879111ac1f44ba5c
Alexander Kuzmin <qrer71@yandex.ru>
2023-03-19 17:19:03 +0300

    Delete tests

--- tests/test_windows.py
+++
@@ -1,9 +0,0 @@
-try:
-    from views.main_window import MainWindow
-except ImportError:
-    assert False, "Не найдено главное окно приложения!"
-
-try:
-    from views.database_window import DatabaseWindow
-except ImportError:
-    assert False, "Не найдено окно базы данных!"
