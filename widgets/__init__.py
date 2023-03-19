c07cc6bb2252526b2ab39f2fac3d43ca5347935d
Alexander Kuzmin <qrer71@yandex.ru>
2023-03-19 17:22:55 +0300

    Add packets with widgets

---
+++ widgets/__init__.py
@@ -0,0 +1,22 @@
+from .widgets import (
+    CentralWidget,
+    ActionWidget,
+    SettingsWindget,
+    ParametersWidget,
+    ResultWidget,
+    DataWidget,
+    ControlWidget,
+    DatabaseWidget,
+)
+
+
+__all__ = [
+    "CentralWidget",
+    "ActionWidget",
+    "SettingsWindget",
+    "ParametersWidget",
+    "ResultWidget",
+    "DataWidget",
+    "ControlWidget",
+    "DatabaseWidget",
+]
