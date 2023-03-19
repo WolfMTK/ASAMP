1116082f6202bd7bb482dd0e1268173db625cefe
Alexander Kuzmin <qrer71@yandex.ru>
2023-03-19 17:14:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
-from views.main_window import run_main_window
+import sys
+
+from PySide6.QtWidgets import QApplication
+
+# from windows import MainWindow
+from windows.main_window import run_main_window
+
+
+# def main() -> None:
+#     app = QApplication(sys.argv)
+#     window = MainWindow()
+#     window.show()
+#     sys.exit(app.exec())
+
 
 if __name__ == "__main__":
     run_main_window()
