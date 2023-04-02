edb211e9250c41d6e2ce244981090da6ca7afac4
Alexander Kuzmin <qrer71@yandex.ru>
2023-04-02 21:48:08 +0300

    Update __init__.py

--- widgets/__init__.py
+++ widgets/__init__.py
@@ -1,13 +1,12 @@
-from .widgets import (
-    CentralWidget,
-    ActionWidget,
-    SettingsWindget,
-    ParametersWidget,
-    ResultWidget,
-    DataWidget,
-    ControlWidget,
+from .database_widget import (
     DatabaseWidget,
 )
+from .central_widget import CentralWidget
+from .action_widget import ActionWidget
+from .settings_widget import SettingsWindget, ParametersWidget
+from .result_widget import ResultWidget
+from .data_widget import DataWidget
+from .control_widget import ControlWidget
 
 
 __all__ = [
