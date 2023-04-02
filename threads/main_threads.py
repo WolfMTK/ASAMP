5be2ed0b74d0bbf5ab99b829bd3f5a9512099036
Alexander Kuzmin <qrer71@yandex.ru>
2023-04-02 22:01:04 +0300

    Create the launch of separate threads so that the program does not hang

---
+++ threads/main_threads.py
@@ -0,0 +1,57 @@
+import logging
+
+from PySide6.QtCore import QThread
+from PySide6.QtWidgets import QComboBox, QTableWidget
+
+from .brand import Brand
+from .type_part import TypePart
+from .material import Material
+from .analysis_parameters import AnalysisParameters
+
+
+logger = logging.getLogger(__name__)
+
+
+class MainThreads(QThread):
+    def __init__(
+        self,
+        combo_box_material: QComboBox,
+        combo_box_brand: QComboBox,
+        combo_box_type_part: QComboBox,
+        table_analysis: QTableWidget,
+    ) -> None:
+        super().__init__()
+        self.combo_box_material = combo_box_material
+        self.combo_box_brand = combo_box_brand
+        self.combo_box_type_part = combo_box_type_part
+        self.table_analysis = table_analysis
+
+    def run(self) -> None:
+        self.__start_thread_material()
+        self.__start_thread_brand()
+        self.__start_thread_type_part()
+        self.__start_thread_analysis_parameters()
+
+    def close_thread(self) -> None:
+        self.material.close_thread()
+        self.brand.close_thread()
+        self.type_part.close_thread()
+        self.analysis_parameters.close_thread()
+
+    def __start_thread_material(self):
+        self.material = Material(self.combo_box_material)
+        self.material.start()
+
+    def __start_thread_brand(self):
+        self.brand = Brand(self.combo_box_brand, self.combo_box_material)
+        self.brand.start()
+
+    def __start_thread_type_part(self):
+        self.type_part = TypePart(self.combo_box_type_part)
+        self.type_part.start()
+
+    def __start_thread_analysis_parameters(self):
+        self.analysis_parameters = AnalysisParameters(
+            self.table_analysis, self.combo_box_type_part
+        )
+        self.analysis_parameters.start()
