3484b23aaa37c18a5d00539315e51c8b11aecfad
Alexander Kuzmin <qrer71@yandex.ru>
2023-04-02 21:43:06 +0300

    Add program startup to the main function

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
 import sys
+import logging
 
 from PySide6.QtWidgets import QApplication
 
-# from windows import MainWindow
-from windows.main_window import run_main_window
+from windows import MainWindow
+from database import Database
 
 
-# def main() -> None:
-#     app = QApplication(sys.argv)
-#     window = MainWindow()
-#     window.show()
-#     sys.exit(app.exec())
+def main() -> None:
+    Database("sql/create_table.sql").create_table()
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
 
 
 if __name__ == "__main__":
-    run_main_window()
+    logging.basicConfig(
+        filename="logs_app.log",
+        level=logging.INFO,
+        format="%(asctime)s, %(levelname)s, %(message)s, %(name)s",
+    )
+    logging.getLogger(__name__).info("Запуск программы!")
+    main()
